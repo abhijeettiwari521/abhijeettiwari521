f1b09806927daebd82bd1b6cf679a72203fe903e
Abhijeettiwari521 <abhijeettiwari521@gmail.com>
2022-06-25 16:50:51 +0530

    day2 completed  hinttext,padding(fromltrb),obscuretext,prefixicon

--- lib/screens/login_screens.dart
+++ lib/screens/login_screens.dart
@@ -12,8 +12,8 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   @override
-  //finalkey
-  // ignore: override_on_non_overriding_member
+  //formkey
+  // ignore: override_on_non_overriding_member, unused_field
   final _formKey = GlobalKey<FormState>();
 //editing controller
   // ignore: unnecessary_new
@@ -22,6 +22,7 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController passwerdController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
+    //email final
     final emailfield = TextFormField(
       autofocus: false,
       controller: emailController,
@@ -31,19 +32,73 @@ class _LoginScreenState extends State<LoginScreen> {
         emailController.text = value!;
       },
       textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefix: Icon(Icons.mail),
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Email",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
     );
+    //password field
     final passwerdfield = TextFormField(
       autofocus: false,
       controller: passwerdController,
+      obscureText: true,
+      //validator:(){}
       onSaved: (value) {
         passwerdController.text = value!;
       },
       textInputAction: TextInputAction.done,
+      decoration: InputDecoration(
+        prefix: Icon(Icons.vpn_key),
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Password",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
     );
-
-    return const Scaffold(
+    //loginbutten
+    final loginButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.redAccent,
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width,
+        onPressed: () {},
+        child: Text(
+          "logIn",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+    return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  emailfield,
+                  passwerdfield,
+                  loginButton,
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
